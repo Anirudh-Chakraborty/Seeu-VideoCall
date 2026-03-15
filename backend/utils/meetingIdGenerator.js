6c672f9fffb284ebb3dd6974a104500eb393ad8c
anirudhchakraborty <preetiujjal@gmail.com>
2026-03-15 20:24:28 +0530

    MeetingId generation Logic changed

--- backend/utils/meetingIdGenerator.js
+++ backend/utils/meetingIdGenerator.js
@@ -1,10 +1,5 @@
+const crypto = require ('crypto');
 function generateMeetingId() {
-  const chars = "abcdefghijklmnopqrstuvwxyz";
-
-  const generatePart = () =>
-    Array.from({ length: 3 }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
-
-  return `${generatePart()}-${generatePart()}-${generatePart()}`;
-}
-
+  const id = "seeu-" + crypto.randomUUID();
+  return id;}
 module.exports = generateMeetingId;
