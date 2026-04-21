19c1e1c5d539cde043d3b9f823b9a429adf76511
anirudhchakraborty <preetiujjal@gmail.com>
2026-04-21 13:12:33 +0530

    App.tsx file changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import VideoRoom from './components/VideoRoom';
+import LandingPage from './pages/LandingPage';
+import PricingPage from './pages/PricingPage';
+import VideoRoom from './pages/VideoRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
         <Route path="/room/:meetingId" element={<VideoRoom />} />
       </Routes>
     </BrowserRouter>
